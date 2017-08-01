613831038ccf930fe9b17be53cd82f1bb40e1e81
shenyuancheng <shenyuanchen@mail.xusenintl.com>
2017-08-01 18:41:47 +0800

    '明天继续'

--- src/components/culture/List.vue
+++ src/components/culture/List.vue
@@ -46,148 +46,14 @@
       </div>
       <div class="class-ctrl">
         <flexbox style="height: 100%" :gutter="0">
-          <flexbox-item class="class-item">
-            <div class="class-i-i">
-              <div class="ci">
-                <i class="fa fa-bars"></i>
-                <span>给你好看</span>
-              </div>
-              <div class="child-menu">
-                <ul>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </flexbox-item>
-          <flexbox-item class="class-item">
-            <div class="class-i-i">
-              <div class="ci">
-                <i class="fa fa-bars"></i>
-                <span>国家相册</span>
-              </div>
-              <div class="child-menu">
-                <ul>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </flexbox-item>
-          <flexbox-item class="class-item">
-            <div class="class-i-i">
-              <div class="ci">
-                <i class="fa fa-bars"></i>
-                <span>活动</span>
-              </div>
-              <div class="child-menu">
-                <ul>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                  <li class="ellipsis">
-                    2017年度颁奖盛典
-                  </li>
-                </ul>
-              </div>
-            </div>
+          <flexbox-item class="class-item" v-for="item in class_list" :key="item.id">
+            <class-block :classList="item" v-on:class-id="getClassId"></class-block>
           </flexbox-item>
         </flexbox>
       </div>
     </div>
 </template>
 <style scoped>
-  .child-menu {
-    position: absolute;
-    bottom: 52px;
-    left: -4px;
-    width: 105%;
-    background: #fff;
-    border: 1px solid #ccc;
-    border-radius: 2px;
-    z-index: 2;
-  }
-  .child-menu ul {
-    padding: 5px 0;
-  }
-  .child-menu ul li {
-    font-size: 12px;
-    padding: 5px 10px;
-    color: #333;
-    text-align: center;
-  }
-  .child-menu ul li:active {
-    background: #ccc;
-  }
-  .class-item {
-    height: 100%;
-  }
-  .class-i-i {
-    position: relative;
-    width: 100%;
-    height: 100%;
-  }
-  .class-i-i:before {
-    content: '';
-    position: absolute;
-    right: 0;
-    top: 0;
-    height: 200%;
-    border-right: 1px solid #ccc;
-    -webkit-transform-origin: 0 0;
-    transform-origin: 0 0;
-    -webkit-transform: scale(.5, .5);
-    transform: scale(.5, .5);
-  }
-  .class-i-i .ci {
-    padding: 15px 0;
-    height: 50px;
-    text-align: center;
-    color: #999;
-  }
-  .class-i-i .ci {
-    font-size: 12px;
-  }
-  .class-item:first-child .class-i-i .child-menu {
-    width: 95%;
-    left: 5px;
-    z-index: 1;
-  }
-  .class-item:last-child .class-i-i .child-menu {
-    width: 95%;
-    left: 0;
-    right: 5px;
-    z-index: 1;
-  }
-  .class-item:last-child .class-i-i:before {
-    border-right: none;
-  }
   .class-ctrl {
     position: fixed;
     bottom: 0;
@@ -266,6 +132,7 @@
 </style>
 <script>
     import { Swiper, SwiperItem, Flexbox, FlexboxItem } from 'vux'
+    import classBlock from './classBlock.com.vue'
     export default {
       data () {
         return {
@@ -330,6 +197,53 @@
               title: '千帆远澋|一夜摩登 住在香榭丽舍的繁华里3',
               content: '全世界夜景最为璀璨的地方，我想巴黎当仁不让。巴黎的时尚和摩登，许多人趋之向往。她是塞纳河畔上流淌着的一场梦，无论是埃菲尔铁塔在高空黑夜里的傲然挺立'
             }
+          ],
+          class_list: [
+            {
+              id: 22,
+              level_one: '给你好看',
+              level_two: [
+                {
+                    id: 1,
+                    title: '2017年度颁奖盛典'
+                },
+                {
+                  id: 2,
+                  title: '2016年度颁奖盛典'
+                },
+                {
+                  id: 3,
+                  title: '2012年度颁奖盛典'
+                },
+                {
+                  id: 4,
+                  title: '2010年度颁奖盛典'
+                }
+              ]
+            },
+            {
+              id: 33,
+              level_one: '国家相册',
+              level_two: []
+            },
+            {
+              id: 44,
+              level_one: '活动',
+              level_two: [
+                {
+                  id: 1,
+                  title: '1997年度颁奖盛典'
+                },
+                {
+                  id: 2,
+                  title: '1998年度颁奖盛典'
+                },
+                {
+                  id: 3,
+                  title: '1999年度颁奖盛典'
+                }
+              ]
+            }
           ]
         }
       },
@@ -337,12 +251,19 @@
         Swiper,
         SwiperItem,
         Flexbox,
-        FlexboxItem
+        FlexboxItem,
+        classBlock
       },
       methods: {
-          change (index) {
-              console.log(index)
-          }
+        change (index) {
+          console.log(index);
+        },
+        getClassId (id) {
+          console.log(id);
+        }
+      },
+      mounted() {
+
       }
     }
 </script>
---
+++ src/components/culture/classBlock.com.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <div class="class-i-i">
+      <div class="ci" v-on:click="cc($event, classList.id)">
+        <i v-if="classList.level_two.length" class="fa fa-bars"></i>
+        <span>{{classList.level_one}}</span>
+      </div>
+      <div class="child-menu" v-if="classList.level_two.length" :class="{ hide: isHide }">
+        <ul>
+          <li class="ellipsis" v-for="item in classList.level_two" v-on:click="cc($event, item.id)">
+            {{ item.title }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+  .child-menu {
+    display: block;
+    position: absolute;
+    bottom: 52px;
+    left: -4px;
+    width: 105%;
+    background: #fff;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    z-index: 2;
+  }
+  .child-menu ul {
+    padding: 5px 0;
+  }
+  .child-menu ul li {
+    font-size: 12px;
+    padding: 5px 10px;
+    color: #333;
+    text-align: center;
+  }
+  .child-menu ul li:active {
+    background: #ccc;
+  }
+  .class-i-i {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  .class-i-i:before {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 200%;
+    border-right: 1px solid #ccc;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scale(.5, .5);
+    transform: scale(.5, .5);
+  }
+  .class-i-i .ci {
+    padding: 15px 0;
+    height: 50px;
+    text-align: center;
+    color: #999;
+  }
+  .class-i-i .ci {
+    font-size: 12px;
+  }
+  .class-item {
+    height: 100%;
+  }
+  .child-menu.hide {
+    display: none;
+  }
+  .class-item:last-child .class-i-i .child-menu {
+    width: 95%;
+    left: 0;
+    right: 5px;
+    z-index: 1;
+  }
+  .class-item:nth-child(1) .class-i-i .child-menu {
+    width: 95%;
+    left: 50%;
+    margin-left: -47.5%;
+    z-index: 1;
+  }
+  .class-item:last-child .class-i-i:before {
+    border-right: none;
+  }
+</style>
+<script>
+    export default{
+        data(){
+            return {
+              isHide: true
+            }
+        },
+        props: [
+            'classList'
+        ],
+        methods: {
+          cc(ev, id) {
+            this.isHide = !this.isHide;
+            if(id) {
+              this.$emit('class-id', id);
+            }else {
+
+            }
+          }
+        },
+        mounted() {
+        }
+    }
+</script>
